94f871e3ae0c025c6708e2dd573aa252b02e80ee
Onkar Chandorikar <onkarchandorikar@gmail.com>
2023-04-01 14:30:08 +0530

    first commit

---
+++ vijay.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class vijay
+{
+    public static void main(String[] args)
+    {
+        String []arr=new String[10];
+        System.out.println("Enter Names");
+        Scanner sc=new Scanner(System.in);
+        arr=sc.nextline();
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+        
+    }
+}
